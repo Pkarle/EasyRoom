751772cb7b028a660296a1a6938e7064d7b8bdb6
BENJAMIN <benjamin.leroy73100@gmail.com>
2019-01-16 15:49:12 +0100

    Create entity, controller, basic view and migration for Proposals

---
+++ src/Repository/ProposalsRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Proposals;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Proposals|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Proposals|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Proposals[]    findAll()
+ * @method Proposals[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProposalsRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Proposals::class);
+    }
+
+    // /**
+    //  * @return Proposals[] Returns an array of Proposals objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Proposals
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
