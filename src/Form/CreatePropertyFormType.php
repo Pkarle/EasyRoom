c23fab13c9e0b6ae613dd22c3aabb4fe0f1c4ff5
BENJAMIN <benjamin.leroy73100@gmail.com>
2019-01-16 11:27:50 +0100

    Create form to create property and simple view

---
+++ src/Form/CreatePropertyFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreatePropertyFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('maxNumberPersons')
+            ->add('postalCode')
+            ->add('region')
+            ->add('country')
+            ->add('typeProperty')
+            ->add('pictures')
+            ->add('surface')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+}
