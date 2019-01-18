ff843a21353527168d0d73bdcf5910bd01295984
digital <digital>
2019-01-18 12:16:05 +0100

    creation d'une réservation pour un bien matérialisé par un formulaire

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\CreateReservationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReservationController extends AbstractController
 {
@@ -15,5 +17,28 @@ class ReservationController extends AbstractController
         return $this->render('reservation/index.html.twig', [
             'controller_name' => 'ReservationController',
         ]);
+    }
+
+    /**
+     * @Route("/reservation/create", name="create_eservation")
+     * @param Request $request
+     * @return Response
+     */
+    public function createReservation(Request $request): Response
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(CreateReservationFormType::class, $reservation );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+        }
+        return $this->render('property/create.reservation.html.twig', [
+            'reservation'=>$reservation,
+            'createReservationForm' => $form->createView(),
+            ]);
     }
 }
